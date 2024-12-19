0f053f05c64beded7b6d0f34ca2b591d27238aa2
Carlgo11 <carlgo11@users.noreply.github.com>
2024-12-20 00:40:25 +0100

    Pass 'registerCommand' as an importable function

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import Server from './core/SMTPServer.js';
 import Logger from './utils/Logger.js';
 import Listen from './core/Event.js';
 import Response from './models/Response.js';
-
+import {registerCommand} from './commands/CommandHandler.js';
 /**
  * @exports SMTPServer
  * @exports Response
  * @exports Listen
  * @exports Logger
  */
-export { Server, Response, Listen, Logger };
+export { Server, Response, Listen, Logger , registerCommand};
